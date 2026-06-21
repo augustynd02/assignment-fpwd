30812a57f26546d3b7b11207b6365c9d58332e4c
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-21 06:16:28 +0000

    refactor(backend/main): use ConfigService instead of process to retrieve environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: configService.get<string>('FRONTEND_URL'),
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 8000);
+
+  const port = configService.get<number>('PORT') ?? 8000;
+  await app.listen(port);
 }
 bootstrap();
