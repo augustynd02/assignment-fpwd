a07224e83c1e482a517de5e7650b741f8c436a6f
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-21 06:12:48 +0000

    fix(backend): properly configure the main app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ExchangeModule } from './exchange/exchange.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ExchangeModule,
+    TransactionModule,
+  ],
 })
 export class AppModule {}
