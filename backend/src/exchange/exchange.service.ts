97d88d0dbe27057a293f91b02328b5586dd5f6f3
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-21 05:30:41 +0000

    feat(exchange): implement caching logic for exchange rate

--- backend/src/exchange/exchange.service.ts
+++ backend/src/exchange/exchange.service.ts
@@ -1,7 +1,14 @@
-import { Injectable, ServiceUnavailableException } from '@nestjs/common';
+import { Injectable, Inject, ServiceUnavailableException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+
+type RateType = {
+  rate: number;
+  timestamp: number;
+};
 
 type ExchangeRateResponse = {
   data: {
@@ -10,14 +17,31 @@ type ExchangeRateResponse = {
   };
 };
 
+const CACHE_KEY = 'exchange_rate';
+const CACHE_TTL = 60 * 1000;
+
 @Injectable()
 export class ExchangeService {
   constructor(
     private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
     private readonly configService: ConfigService,
   ) {}
 
   async getExchangeRate(): Promise<ExchangeRateResponse> {
+    const cachedData = await this.cacheManager.get<RateType>(CACHE_KEY);
+    if (cachedData) {
+      const dataAge = Date.now() - cachedData.timestamp;
+      const secondsLeft = Math.max(0, 60 - Math.floor(dataAge / 1000));
+
+      return {
+        data: {
+          rate: cachedData.rate,
+          secondsLeft,
+        },
+      };
+    }
+
     const apiUrl = this.configService.getOrThrow<string>('API_URL');
     const apiKey = this.configService.getOrThrow<string>('API_KEY');
 
@@ -34,6 +58,9 @@ export class ExchangeService {
         'Could not retrieve exchange rate. Please try again later.',
       );
     }
+
+    const timestamp = Date.now();
+    await this.cacheManager.set(CACHE_KEY, { rate, timestamp }, CACHE_TTL);
 
     return {
       data: {
