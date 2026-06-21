23e4fabf0c822418a4e304c887435d33de81ea09
Dominik Augustyn <110547061+augustynd02@users.noreply.github.com>
2026-06-21 06:10:40 +0000

    feat(main): configure CORS in main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
